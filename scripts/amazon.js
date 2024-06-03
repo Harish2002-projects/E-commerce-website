dcb85715f7f9acc2205aae102ca04dca2663e9ab
harish <happyharish2002@gmail.com>
2024-06-03 17:24:14 +0530

    fixed checkout page cart quantity and correctly update the items on click

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,7 +72,7 @@ function renderProductsGrid() {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -100,6 +100,19 @@ function renderProductsGrid() {
   document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
       const productId = button.dataset.productId;
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      addedMessage.classList.add("added-to-cart-visible");
+      let addedMessageTimeoutId;
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove("added-to-cart-visible");
+      }, 2000);
+      addedMessageTimeoutId = timeoutId;
       const quantitySelector = document.querySelector(
         `.js-quantity-selector-${productId}`
       );
