60b03b235c8acc97f822699cb6f5839c1c45f266
harish <happyharish2002@gmail.com>
2024-06-03 20:48:35 +0530

    name changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -123,7 +123,7 @@ function renderProductsGrid() {
 
   document.querySelector(".js-search-button").addEventListener("click", () => {
     const search = document.querySelector(".js-search-bar").value;
-    window.location.href = `amazon.html?search=${search}`;
+    window.location.href = `index.html?search=${search}`;
   });
 
   document
@@ -131,7 +131,7 @@ function renderProductsGrid() {
     .addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
         const searchTerm = document.querySelector(".js-search-bar").value;
-        window.location.href = `amazon.html?search=${searchTerm}`;
+        window.location.href = `index.html?search=${searchTerm}`;
       }
     });
   updateCartQuantity();
